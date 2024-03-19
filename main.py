d6dbc82a83328f0a6ad94fdd778e5d498388de5a
NickSpv <pstebenev@internet.ru>
2024-03-19 11:23:30 +0300

    upd

--- main.py
+++ main.py
@@ -7,6 +7,56 @@ np.random.seed(0)
 import time
 
 
+class OdinokiyOdinochka(ThreeDScene):
+    def construct(self):
+        axes = ThreeDAxes()
+        square = Square(color=BLUE, side_length=2)
+        dot1 = Dot(color=BLUE).shift(UP).shift(LEFT)
+        dot2 = Dot(color=BLUE).shift(DOWN).shift(LEFT)
+        cylinder = Cylinder(resolution=(100,100), radius=1, height=2)
+        cylinder.rotate(angle=PI/2, axis=LEFT)
+        cylinder.set_color(RED)
+        cylinder.set_stroke(GREEN)
+        all = VGroup(square, dot1, dot2)
+        trace1 = TracedPath(dot1.get_begin)
+        trace2 = TracedPath(dot2.get_begin)
+        self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
+        self.add(axes)
+        self.begin_ambient_camera_rotation(90*DEGREES/3)
+        self.play(Create(square))
+        self.add(dot1, dot2, trace1, trace2)
+        self.play(
+            # Rotate(dot1, angle=PI, about_point=ORIGIN, axis=UP),
+            # Rotate(dot2, angle=PI, about_point=ORIGIN, axis=UP), 
+            Rotate(all, angle=2*PI, axis=UP), run_time=10
+        )
+        self.play(FadeIn(cylinder))
+        self.wait(2)
+        self.stop_ambient_camera_rotation()
+
+
+class OdinokiyOdinochkaIshshoOdin(ThreeDScene):
+    def construct(self):
+        axes = ThreeDAxes()
+        square = Square(color=BLUE, side_length=2)
+        dot1 = Dot(color=BLUE).shift(UP).shift(LEFT)
+        dot2 = Dot(color=BLUE).shift(DOWN).shift(LEFT)
+        all = VGroup(square, dot1, dot2)
+        trace1 = TracedPath(dot1.get_start)
+        trace2 = TracedPath(dot2.get_start)
+        self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
+        self.add(axes)
+        self.begin_ambient_camera_rotation(90*DEGREES/3)
+        self.play(Create(square))
+        self.add(dot1, dot2, trace1, trace2)
+        self.play(
+            # Rotate(dot1, angle=PI, about_point=ORIGIN, axis=UP),
+            # Rotate(dot2, angle=PI, about_point=ORIGIN, axis=UP), 
+            Rotate(all, angle=2*PI, axis=UP), run_time=10
+        )
+        self.wait(2)
+        self.stop_ambient_camera_rotation()
+
 class Part0(Scene):
     def construct(self):
         intro_text_1 = Text('Это видео демонстрирует', should_center=True)
